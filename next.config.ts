fff7053c45e9ba7fee63570b35bcaef46d5eab83
Myo Myint Myat <kairi31myomyat@gmail.com>
2026-06-10 14:52:33 +0700

    upload the course image from the device and url.

--- next.config.ts
+++ next.config.ts
@@ -23,6 +23,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "*.supabase.co",
       },
+      {
+        protocol: "https",
+        hostname: "*.gstatic.com",
+      },
     ],
   },
 };
